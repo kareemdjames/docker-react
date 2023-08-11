63f6c820cd3edc80f9ab12e1ee939862bb0f863b
Kareem <kareemdjames@gmail.com>
2023-08-11 12:55:51 -0400

    created new create react app, added docker files for dev and prod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello, stay a while and listen! Ya flithy animal lol! I didn't really mean that, no
         </p>
         <a
           className="App-link"
